e8c5bdff3027d24700b01fe0da590b4c3472fa9e
HelloRWA <dev@rwa-nft.com>
2024-07-05 16:02:44 +0800

    🎉 begin

---
+++ pages/login.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+const { auth } = useSupabaseClient();
+
+const doLogin = async () => {
+  const redirectTo = `${location.origin}/confirm`;
+  await auth.signInWithOAuth({
+    provider: "twitter",
+    options: {
+      redirectTo,
+    },
+  });
+};
+</script>
+
+<template>
+  <div>
+    <button @click="doLogin">Sign in with Twitter</button>
+  </div>
+</template>
